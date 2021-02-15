ed291aa804937b378202ac4b66eeeb234da02266
Decalin <sallyiam00@gmail.com>
2021-02-15 11:50:37 +0900

    add

--- Python/Delivery.py
+++ Python/Delivery.py
@@ -1,14 +1,14 @@
 def underSix(count) : #6개 미만 가격
-    tot = cup * count
+    tot = 0
     if count == 5:
         tot += bag_4 + carrier * 3
-    else if count == 4:
+    elif count == 4:
         tot += bag_4 + carrier * 2
-    else if count == 3:
+    elif count == 3:
         tot += bag_2 + bag_1
-    else if count == 2:
+    elif count == 2:
         tot += bag_2
-    else if count == 1:
+    elif count == 1:
         tot += bag_1
     return tot
 
@@ -20,16 +20,18 @@ cup = 100
 bag_4 = 80
 bag_1 = 25
 
-order_count = input()
+order_count = 5
 
 total = order_count * cup
+print("컵 %d원", % total)
 
 if order_count // 6 >= 1 :
     bag6Count = order_count // 6
     total += bag6Count * (bag_4 + carrier *3)
+    #print("대짜 쇼핑백 %d원, 캐리어 %d원", %(bag6Count * bag_4, bag6Count * carrier * 3))
     
     total += underSix(order_count % 6)
 else :
     total += underSix(order_count % 6) 
     
-print("음료 포장값: %d", % total)
+print("총액: %d", %total)
